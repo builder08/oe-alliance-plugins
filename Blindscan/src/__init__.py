919e52ca3143c460b96e279b7aa38dd29d3239ce
builder08 <75539906+builder08@users.noreply.github.com>
2020-12-05 23:47:50 +0200

    Add files via upload

--- Blindscan/src/__init__.py
+++ Blindscan/src/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function
 from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
-import os, gettext
+import gettext
 
 PluginLanguageDomain = "Blindscan"
 PluginLanguagePath = "SystemPlugins/Blindscan/locale"
@@ -14,7 +13,7 @@ def _(txt):
 	if gettext.dgettext(PluginLanguageDomain, txt):
 		return gettext.dgettext(PluginLanguageDomain, txt)
 	else:
-		print("[" + PluginLanguageDomain + "] fallback to default translation for " + txt)
+		#print "[" + PluginLanguageDomain + "] fallback to default translation for " + txt
 		return gettext.gettext(txt)
 
 language.addCallback(localeInit())
